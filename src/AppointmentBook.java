361a04a5aae1a78e3cbbd7b8d0fa66bc54914ea2
AustinArvelo <austina46@nycstudents.net>
2025-09-26 13:20:20 -0400

    created appointment Book b

--- src/AppointmentBook.java
+++ src/AppointmentBook.java
@@ -6,13 +6,22 @@ public class AppointmentBook {
     private boolean isMinuteFree(int period, int minute){
         return schedule[period - 1][minute];
     }
-    public int findFreeBlock(int period, int duration){
-        return 0;
-    }
     public boolean makeAppointment(int startPeriod, int endPeriod, int duration){
         return false;
     }
     public void printPeriod(int period){
         for(int i = 0; i < schedule[period - 1].length; i++) System.out.println(i + " " + schedule[period - 1][i]);
+    }
+    public int findFreeBlock(int period, int duration) {
+        int block = 0;
+        for(int i = 0; i < 60; i++){
+            if(isMinuteFree(period, i)){
+                block++;
+                if(block == duration){
+                    return i - duration + 1;
+                }
+            } else block = 0;
+        }
+        return -1;
     }
 }
