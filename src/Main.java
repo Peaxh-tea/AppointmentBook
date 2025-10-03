5f4b3b4840ec00594d72eed3d5c98f1483ff29c6
AustinArvelo <austina46@nycstudents.net>
2025-10-03 13:12:25 -0400

    added test cases

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,9 @@ public class Main {
     public static void main(String[] args){
         boolean[][] schedule = new boolean [8][60];
         AppointmentBook a = new AppointmentBook(schedule);
-//        for(int i = 10; i < 15; i++) schedule[1][i] = true;
-//        for(int i = 30; i < 45; i++) schedule[1][i] = true;
-//        for(int i = 50; i < 60; i++) schedule[1][i] = true;
+        for(int i = 10; i < 15; i++) schedule[1][i] = true;
+        for(int i = 30; i < 45; i++) schedule[1][i] = true;
+        for(int i = 50; i < 60; i++) schedule[1][i] = true;
 //        a.printPeriod(2);
         schedule = new boolean[8][60];
         for(int i = 25; i < 30; i++) schedule[1][i] = true;
@@ -19,8 +19,15 @@ public class Main {
 //            b.printPeriod(period);
 //            period++;
 //        }
+//        b.printPeriod(4);
+        System.out.println(a.findFreeBlock(2,15));
+        System.out.println(a.findFreeBlock(2,9));
+        System.out.println(a.findFreeBlock(2,20));
+        System.out.println(b.makeAppointment(2,4, 22));
         b.printPeriod(4);
-        System.out.println(b.makeAppointment(2,4,22));
-        b.printPeriod(4);
+        System.out.println(b.makeAppointment(3,4, 3));
+        b.printPeriod(3);
+        System.out.println(b.makeAppointment(2,4, 30));
+//        b.printPeriod(3);
     }
 }
